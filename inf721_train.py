768c5852d89b366dc6f8a4ff6045cadbd1c00e67
djangotraining <60931865+djangotraining@users.noreply.github.com>
2023-12-12 19:26:47 -0300

    Update inf721_train.py

--- inf721_train.py
+++ inf721_train.py
@@ -1,35 +1,31 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import log_loss
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import pandas as pd
-
-from inf721_dataset import Database, SMILESDataset
+from inf721_dataset import Database
 from inf721_model import PyTorchModel
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
 
-dataset = 'finalData.csv'
+
+# PyTorch Dataset
+class SMILESDataset(Dataset):
+    def __init__(self, sequences, labels):
+        self.sequences = torch.LongTensor(sequences)
+        self.labels = torch.FloatTensor(labels)
+
+    def __len__(self):
+        return len(self.sequences)
+
+    def __getitem__(self, idx):
+        return self.sequences[idx], self.labels[idx]
+
+
+
 
 # Creating an instance of the class
-X, y = Database(dataset)
-
-
-# Tokenize the SMILES strings
-tokenizer = Tokenizer(char_level=True)
-tokenizer.fit_on_texts(X)
-X_encoded = tokenizer.texts_to_sequences(X)
-
-# Pad sequences to have the same length
-X_padded = pad_sequences(X_encoded)
-
-# Label encode the target variable
-label_encoder = LabelEncoder()
-y_encoded = label_encoder.fit_transform(y)
+X_padded, y_encoded, vocab_size = Database('finalData.csv')
 
 # Data Splitting
 X_train, X_test, y_train, y_test = train_test_split(X_padded, y_encoded, test_size=0.2, random_state=42)
@@ -38,20 +34,18 @@ X_train, X_test, y_train, y_test = train_test_split(X_padded, y_encoded, test_si
 train_dataset = SMILESDataset(X_train, y_train)
 test_dataset = SMILESDataset(X_test, y_test)
 
-
-
 batch_size = 32
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-
-# Assuming tokenizer is a variable that holds your tokenizer
-vocab_size = len(tokenizer.word_index) + 1
+# Instantiate the PyTorch model
+input_dim = vocab_size
 embedding_dim = 64
-lstm_units = 32
+hidden_dim = 32
 
 # Instantiate the PyTorch model
-pytorch_model = PyTorchModel(vocab_size, embedding_dim, lstm_units)
+pytorch_model = PyTorchModel(input_dim, embedding_dim, hidden_dim)
+
 
 # PyTorch Loss function and optimizer
 criterion = nn.BCELoss()
@@ -62,15 +56,16 @@ train_losses = []
 val_losses = []
 
 # Training loop
-epochs = 30
+epochs = 10
 for epoch in range(epochs):
-    pytorch_model.train()
-    for inputs, labels in train_loader:
-        inputs, labels = Variable(inputs), Variable(labels)      
+    #pytorch_model.train()
+    for inputs, labels in train_loader:      
         pytorch_model.zero_grad()
         optimizer.zero_grad()
         outputs = pytorch_model(inputs)
+        #print(outputs.size())
         labels = labels.unsqueeze(1)
+        #print(labels.size())
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
