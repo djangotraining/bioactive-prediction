a8e00c221910df38f990d5b90cc70a90e5e19f60
djangotraining <60931865+djangotraining@users.noreply.github.com>
2023-12-12 19:25:42 -0300

    Update inf721_inference.py

--- inf721_inference.py
+++ inf721_inference.py
@@ -1,25 +1,35 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-from torch.autograd import Variable
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import log_loss
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import pandas as pd
+from inf721_model import PyTorchModel
+from inf721_dataset import Database
 
-# Create an instance of the model
-pytorch_model = PyTorchModel(vocab_size, embedding_dim, lstm_units, output_dim)
+
+# Instantiate the PyTorch model
+X_padded, y_encoded, vocab_size = Database('finalData.csv')
+input_dim = vocab_size
+embedding_dim = 64
+hidden_dim = 32
+
+# Creating an instance of the class
+pytorch_model = PyTorchModel(input_dim, embedding_dim, hidden_dim)
 
 # Load the saved model weights
-model_weights_path = 'model_weights_epoch_30.pt'  # Adjust the filename based on your saved weights
+model_weights_path = 'model_weights_epoch_10.pt'  # Adjust the filename based on your saved weights
 pytorch_model.load_state_dict(torch.load(model_weights_path))
 pytorch_model.eval()  # Set the model to evaluation mode
 
 # Perform inference on new data
-new_data = torch.LongTensor(new_sequences)  # Assuming new_sequences is your new data
+new_data = torch.LongTensor(X_padded[1]) 
+
+# Add batch dimension as the first dimension
+new_data = new_data.unsqueeze(0)
+
 with torch.no_grad():
     predictions = pytorch_model(new_data)
+    print(predictions)
+    if predictions >= 0.5:
+        print('active')
+    else:
+        print('Inactive')
 
+# Convert predictions to numpy array for further analysis or use in scikit-learn
+#predictions_np = predictions.numpy()
